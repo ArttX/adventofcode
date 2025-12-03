38e66f039adcc5df614dbb878896fcf4c3993bb2
ArttX <arttxiam@gmail.com>
2025-12-03 21:19:03 +0200

    day 1

---
+++ 2025/day_01/day_01_2.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() async {
+  final file = File("day_01/input.txt");
+  final instructions = await file.readAsLines();
+
+  int current = 50;
+  int count = 0;
+  for (final inst in instructions) {
+    final num = int.parse(inst.substring(1));
+    // Make rotation
+    if (inst.startsWith("R")) {
+      for (int i = num; i > 0; i--) {
+        current += 1;
+        if (current > 99) current = 0;
+        if (current == 0) count++;
+      }
+    }
+    if (inst.startsWith("L")) {
+      for (int i = num; i > 0; i--) {
+        current -= 1;
+        if (current < 0) current = 99;
+        if (current == 0) count++;
+      }
+    }
+  }
+  print("Count: $count");
+}
